859f13ac697e04cf8d2a05ffb38cabadc14e6f41
Scott Land <101.scott@gmail.com>
2017-01-24 23:25:45 -0600

    now encrypts the message provided by the rot given

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 #!/usr/bin/env python
 import webapp2
+import cgi
 import caesar
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = 'Hello world!!!!!!!!!'
-        rot = 13
-        encrypted_message = caesar.encrypt(message, rot)
         header = """
-        <form method="GET">
+        <form method="POST">
         <label>Amount to rotate by:
-        <input type="text" name="rot" />
+        <input type="text" name="rot"/>
         </label>
         <br />
         """
-        boxes = '<textarea name="message" style="height: 100px; width: 400px;">' + encrypted_message + '</textarea>'
+        mainbox = """<textarea name="message"
+        style="height: 100px; width: 400px;"></textarea>
+        """
         footer = """
-        <input type="Submit" />
         <br />
+        <input type="Submit" />
         </form>
         """
-        content = header + boxes + footer
+        content = header + mainbox + footer
+        self.response.write(content)
+
+    def post(self):
+        message = self.request.get("message")
+        rot = self.request.get("rot")
+        if message and rot.isdigit():
+            encrypted_message = caesar.encrypt(message, int(rot))
+        else:
+            encrypted_message = message
+        encrypted_message = cgi.escape(encrypted_message, quote=True)
+        header = """
+        <form method="POST">
+        <label>Amount to rotate by:
+        <input type="text" name="rot" value="%s"/>
+        </label>
+        <br />
+        """ % rot
+        mainbox = """<textarea name="message"
+        style="height: 100px; width: 400px;">%s</textarea>
+        """ % encrypted_message
+        footer = """
+        <br />
+        <input type="Submit" />
+        </form>
+        """
+        content = header + mainbox + footer
         self.response.write(content)
 
 app = webapp2.WSGIApplication([
