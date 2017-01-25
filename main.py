a18b7caac6430c650cb35dbb6c3773e980c4a246
Scott Land <101.scott@gmail.com>
2017-01-24 23:58:11 -0600

    Made it prettier

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ import caesar
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         header = """
+        <h2>Web Caesar</h2>
         <form method="POST">
-        <label>Amount to rotate by:
-        <input type="text" name="rot"/>
+        <label>Rotate by:<br />
+        <input type="number" name="rot"/>
         </label>
-        <br />
+        <p />
         """
-        mainbox = """<textarea name="message"
-        style="height: 100px; width: 400px;"></textarea>
+        mainbox = """<label>Type a message:<br /><textarea name="message"
+        style="height: 100px; width: 400px;"></textarea></label>
         """
         footer = """
-        <br />
+        <p />
         <input type="Submit" />
         </form>
         """
@@ -33,17 +34,18 @@ class MainHandler(webapp2.RequestHandler):
             encrypted_message = message
         encrypted_message = cgi.escape(encrypted_message, quote=True)
         header = """
+        <h2>Web Caesar</h2>
         <form method="POST">
-        <label>Amount to rotate by:
-        <input type="text" name="rot" value="%s"/>
+        <label>Rotate by:
+        <input type="number" name="rot" value="%s"/><br />
         </label>
-        <br />
+        <p />
         """ % rot
-        mainbox = """<textarea name="message"
-        style="height: 100px; width: 400px;">%s</textarea>
+        mainbox = """<label>Type a message:<br /><textarea name="message"
+        style="height: 100px; width: 400px;">%s</textarea></label>
         """ % encrypted_message
         footer = """
-        <br />
+        <p />
         <input type="Submit" />
         </form>
         """
